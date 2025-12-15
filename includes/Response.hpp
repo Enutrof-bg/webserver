f9f05591e4e73e88844d7141555d806283317ea9
Kevin Wang <kevwang@f6r9s14.clusters.42paris.fr>
2025-12-15 22:22:10 +0100

    add method POST fonctionne mais quelque detail a regler

--- includes/Response.hpp
+++ includes/Response.hpp
@@ -18,8 +18,9 @@ struct Response
 Response parseRequest(const std::string &request);
 std::string getPath(const std::string &url, const ServerConfig &server);
 std::string getRequest(const Response &rep, const ServerConfig &server);
-std::string handleGET(const std::string &path, const ServerConfig &server);
 
+std::string handleGET(const std::string &path, const ServerConfig &server);
+std::string handlePOST(const Response &rep, const ServerConfig &server);
 // {
 // 	Response rep;
 // 	std::istringstream stream(request);
