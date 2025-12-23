ba72b2ecb23eb3cb62ae37019b09d8f2f5bb8c57
Kevin Wang <kevwang@f6r9s15.clusters.42paris.fr>
2025-12-23 13:49:24 +0100

    add: start cgi hello.py basics

---
+++ html/hello.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+print("Content-Type: text/html\r\n\r\n")
+print("<html><body>")
+print("<h1>Hello from CGI!</h1>")
+print("<a title=\"Motherfucking Website\" href=\"index.html\">go back</a></p>")
+print("</body></html>")
